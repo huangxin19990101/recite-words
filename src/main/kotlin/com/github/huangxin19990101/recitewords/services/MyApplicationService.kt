58238441e36278c5556cf76cf92549ad755d6bed
GitHub Action <action@github.com>
2022-09-05 07:53:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/huangxin19990101/recitewords/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/huangxin19990101/recitewords/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.huangxin19990101.recitewords.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.huangxin19990101.recitewords.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/huangxin19990101/recitewords/services/MyProjectService.kt
+++ src/main/kotlin/com/github/huangxin19990101/recitewords/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.huangxin19990101.recitewords.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.huangxin19990101.recitewords.MyBundle
 
 class MyProjectService(project: Project) {
 
